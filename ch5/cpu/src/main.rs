900d3f00cf893217281637cbb5cb4c563ce87fdb
psyashes <43512814+psyashes@users.noreply.github.com>
2021-12-24 08:40:00 +0900

    Initilize cpu

--- ch5/cpu/src/main.rs
+++ ch5/cpu/src/main.rs
@@ -2,3 +2,14 @@ struct CPU {
     current_operation: u16,
     registers: [u8; 2],
 }
+
+fn main() {
+    let mut cpu = CPU {
+        current_operation: 0,
+        registers: [0; 2],
+    };
+
+    cpu.current_operation = 0x8014;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+}
