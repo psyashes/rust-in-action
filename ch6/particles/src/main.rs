fd0e754dbd34e2225a15b3f5a4117f883e64f986
psyashes <43512814+psyashes@users.noreply.github.com>
2021-12-30 21:09:22 +0900

    Add world and particle

--- ch6/particles/src/main.rs
+++ ch6/particles/src/main.rs
@@ -4,10 +4,41 @@ use rand::prelude::*;
 use std::alloc::{GlobalAlloc, Layout, System};
 use std::time::Instant;
 
+struct World {
+    current_turn: u64,
+    particles: Vec::<Box<Particle>>,
+    height: f64,
+    width: f64,
+    rng: ThreadRng,
+}
+
+struct Particle {
+    height: f64,
+    width: f64,
+    position: Vec2d<f64>,
+    velocity: Vec2d<f64>,
+    acceleration: Vec2d<f64>,
+    color: [f32; 4],
+}
+
+impl World {
+    fn new(width: f64, height: f64) -> World {
+        World {
+            current_turn: 0,
+            particles: Vec::<Box<Particle>>::new(),
+            height: height,
+            width: width,
+            rng: thread_rng(),
+        }
+    }
+}
+
 fn main() {
     let (width, height) = (1280.0, 960.0);
     let mut window: PistonWindow = WindowSettings::new("particles", [width, height])
         .exit_on_esc(true)
         .build()
         .expect("Could not create a window.");
+
+    let mut world = World::new(width, height);
 }
