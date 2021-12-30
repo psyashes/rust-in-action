ca8ef3aae17e1976b3ea22e167f0ecd29d2c8301
psyashes <43512814+psyashes@users.noreply.github.com>
2021-12-30 20:57:40 +0900

    Setting piston window

--- ch6/particles/src/main.rs
+++ ch6/particles/src/main.rs
@@ -1,3 +1,13 @@
+use graphics::math::{add, mul_scalar, Vec2d};
+use piston_window::*;
+use rand::prelude::*;
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::time::Instant;
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (1280.0, 960.0);
+    let mut window: PistonWindow = WindowSettings::new("particles", [width, height])
+        .exit_on_esc(true)
+        .build()
+        .expect("Could not create a window.");
 }
