a3407995e977552fb7eb0ba32e36e3cd0e726d1c
psyashes <43512814+psyashes@users.noreply.github.com>
2022-01-02 12:19:09 +0900

    Add structs

---
+++ ch7/actionkv/src/lib.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter, SeekFrom};
+use std::path::Path;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use crc::crc32;
+use serde_derive::{Deserialize, Serialize};
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct ActionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
